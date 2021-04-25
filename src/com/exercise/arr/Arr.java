d59f14d70524e44ca34f75770a0032de3f9e9d6e
weibiao0105 <weibiao0105@126.com>
2021-04-25 22:42:06 +0800

    java基础-数组练习

---
+++ src/com/exercise/arr/Arr.java
@@ -0,0 +1,18 @@
+package com.exercise.arr;
+
+/**
+ * 数组：数组是一种用于存储多个相同类型数据的存储模型
+ *
+ * @author Way on 2021/4/25
+ */
+public class Arr {
+
+    public static void main(String[] args) {
+
+        //数组定义格式
+        //数组初始化之动态初始化
+        //内存分配
+        //静态初始化
+        //遍历
+    }
+}
